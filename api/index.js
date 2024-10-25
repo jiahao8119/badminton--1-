9b90d768dcc82e89df58668a5c87748a7b4719e7
yap <jiahaoyap9139@gmail.com>
2024-10-25 21:22:17 +0800

    update

--- api/index.js
+++ api/index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
