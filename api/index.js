6d13e6639b474c15632c41aee365b5ac127868aa
yap <jiahaoyap9139@gmail.com>
2024-10-26 00:16:07 +0800

    tryagain

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: ['https://final-fullstack-vert.vercel.app', 'http://localhost:5173'],
+    origin: ['https://final-fullstack-vert.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
